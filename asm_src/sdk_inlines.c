edfe4ec405ebd5b2fc4cc41920bc9ad57e347a85
Tony Smith <smittytone@icloud.com>
2022-06-17 11:52:41 +1000

    Latest upload @ Fri 17 Jun 2022 11:52:41 AEST

---
+++ asm_src/sdk_inlines.c
@@ -0,0 +1,25 @@
+/*
+ * pico-asm for Raspberry Pi Pico
+ *
+ * @version     1.0.0
+ * @author      smittytone
+ * @copyright   2022
+ * @licence     MIT
+ *
+ */
+
+
+#include "hardware/gpio.h"
+
+/*
+ * These functions are placeholders for SDK functions
+ * that are marked as inline, ie. to be compiled in place.
+ */
+
+void gpio_set_direction(int pin, int direction) {
+    gpio_set_dir(pin, direction);
+}
+
+void gpio_set_state(int pin, int state) {
+    gpio_put(pin, state);
+}
